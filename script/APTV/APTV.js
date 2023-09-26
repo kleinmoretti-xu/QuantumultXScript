746a01d84cdf5941cb1f5dbf95edd29d6de0a36d
klein <kleinmoretti.xu@gmail.com>
2023-09-26 11:09:44 +0800

    APTV解锁

--- script/APTV/APTV.js
+++ script/APTV/APTV.js
@@ -1,7 +1,7 @@
 /**********************************************
  Aptv解锁永久订阅
  [rewrite_local]
- ^https?:\/\/api\.revenuecat\.com/v\d/(subscribers/(\$RCAnonymousID\%)?(\w)+|receipts)$ url script-echo-response https://raw.githubusercontent.com/Crazy-Z7/Script/main/aptv.js
+ ^https?:\/\/api\.revenuecat\.com/v\d/(subscribers/(\$RCAnonymousID\%)?(\w)+|receipts)$ url script-echo-response https://raw.githubusercontent.com/xuzhencai/QuantumultXScript/main/script/APTV/APTV.js
  hostname = api.revenuecat.com
  **********************************************/
 let result = {
