f96b567a358f1d55bfa245b2e6997dd7bbae998a
Klein Moretti <kleinmoretti.xu@gmail.com>
2024-01-13 22:33:00 +0800

    imaotai

--- task/imaotai/imaotai.js
+++ task/imaotai/imaotai.js
@@ -7,10 +7,10 @@
  [mitm]
  hostname = app.moutai519.com.cn
  [rewrite_local]
- https://app.moutai519.com.cn/xhr/front/user/info url script-response-body https://raw.githubusercontent.com/Yuheng0101/X/main/Tasks/imaotai/imaotai.js
+ https://app.moutai519.com.cn/xhr/front/user/info url script-response-body https://raw.githubusercontent.com/kleinmoretti-xu/QuantumultXScript/main/task/imaotai/imaotai.js
  [task_local]
  # 茅台自动预约
- 0 9 * * * https://raw.githubusercontent.com/Yuheng0101/X/main/Tasks/imaotai/imaotai.js, tag=i茅台自动预约, img-url=https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/ae/f4/18/aef41811-955e-e6b0-5d23-6763c2eef1ab/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/144x144.png, enabled=true
+ 0 9 * * * https://raw.githubusercontent.com/kleinmoretti-xu/QuantumultXScript/main/task/imaotai/imaotai.js, tag=i茅台自动预约, img-url=https://raw.githubusercontent.com/kleinmoretti-xu/QuantumultXScript/main/icon/imaotai.png, enabled=true
  ******************************************/
 var $ = new Env('i茅台'), service = $.http
 // 开发方便兼容node
